647cf2bf1fdbf5127165e0524e28ee22841178e5
Marina Savina <fighter_61@bk.ru>
2022-11-22 20:21:51 +0300

    Максимум ииз 9ти через массив

---
+++ Exmpl009/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max(int arg1,int arg2,int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+ int []array = {8,0,72,156,4,74,15,89,23};
+
+ int max = Max(
+    Max(array[0], array[1], array[3]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+ );
+
+Console.WriteLine(max);
